9e0397d0545dc09afd224c9b4c0dacaf556bd3ee
David Valbuena <davidvalbuena99@gmail.com>
2021-07-01 17:27:27 -0500

    added methods to get translation from array of data

--- src/Models/Tenant.php
+++ src/Models/Tenant.php
@@ -6,7 +6,7 @@ use Placetopay\Cerberus\Scopes\AppScope;
 use Spatie\Multitenancy\Models\Tenant as TenantSpatie;
 
 /**
- * Class Tenant
+ * Class Tenant.
  * @property string name
  * @property string app
  * @property string domain
@@ -30,5 +30,41 @@ class Tenant extends TenantSpatie
         parent::boot();
 
         static::addGlobalScope(new AppScope());
+    }
+
+    public function translate(string $key): string
+    {
+        $locale = $this->normalizeLocale($key);
+        return $this->getTranslations($key)[$locale] ?? '';
+    }
+
+    private function getTranslations(string $key): array
+    {
+        return config("tenant.{$key}", []);
+    }
+
+    private function normalizeLocale(string $key): ?string
+    {
+        $translatedLocales = array_keys($this->getTranslations($key));
+        $locale = app()->getLocale();
+
+        if (in_array($locale, $translatedLocales)) {
+            return $locale;
+        }
+
+        if (in_array($this->shortLocale($locale), $translatedLocales)) {
+            return $this->shortLocale($locale);
+        }
+
+        if (!is_null(app()->getFallbackLocale())) {
+            return app()->getFallbackLocale();
+        }
+
+        return $locale;
+    }
+
+    private function shortLocale($locale): string
+    {
+        return strtolower(substr($locale, 0, 2));
     }
 }
