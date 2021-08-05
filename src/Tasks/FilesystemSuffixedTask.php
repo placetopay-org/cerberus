24ac37a1bbd03ca164f43b209b746cefd9aa23cd
David Valbuena <davidvalbuena99@gmail.com>
2021-08-05 13:02:49 -0500

    adds validation for suffix storage

--- src/Tasks/FilesystemSuffixedTask.php
+++ src/Tasks/FilesystemSuffixedTask.php
@@ -64,6 +64,11 @@ class FilesystemSuffixedTask implements SwitchTenantTask
 
         // Storage facade
         foreach (config('multitenancy.filesystems_disks') as $disk) {
+
+            if (!$this->canSuffixS3Driver($disk)) {
+                continue;
+            }
+
             /** @var FilesystemAdapter $filesystemDisk */
             $filesystemDisk = Storage::disk($disk);
 
