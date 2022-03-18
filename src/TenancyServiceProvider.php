5cbced81603050610627f0650adc14faf4bffc3f
Juan Cardona <juan.cardona@evertecinc.com>
2022-03-18 16:34:39 -0500

    refactor: override new parent package methods

--- src/TenancyServiceProvider.php
+++ src/TenancyServiceProvider.php
@@ -10,27 +10,30 @@ use Placetopay\Cerberus\Commands\TenantsSkeletonStorageCommand;
 use Placetopay\Cerberus\Http\Controllers\TenantController;
 use Placetopay\Cerberus\Http\Middlewares\AppCleanCache;
 use Placetopay\Cerberus\Listeners\SetLoggerContext;
-use Spatie\Multitenancy\Actions\MakeQueueTenantAwareAction;
+use Spatie\LaravelPackageTools\Package;
 use Spatie\Multitenancy\Events\MadeTenantCurrentEvent;
-use Spatie\Multitenancy\Exceptions\InvalidConfiguration;
 use Spatie\Multitenancy\MultitenancyServiceProvider;
-use Spatie\Multitenancy\Tasks\TasksCollection;
-use Spatie\Multitenancy\TenantFinder\TenantFinder;
 
 class TenancyServiceProvider extends MultitenancyServiceProvider
 {
-    protected function bootCommands(): self
+    public function configurePackage(Package $package): void
     {
-        $this->commands([
-            TenantsArtisanCommand::class,
-            TenantsListCommand::class,
-            TenantsSkeletonStorageCommand::class,
-        ]);
-
-        return $this;
+        $package
+            ->name('laravel-multitenancy')
+            ->hasConfigFile()
+            ->hasMigrations([
+                'landlord/create_landlord_tenants_table',
+                'landlord/create_landlord_jobs_table',
+                'landlord/create_landlord_failed_jobs_table',
+            ])
+            ->hasCommands([
+                TenantsArtisanCommand::class,
+                TenantsListCommand::class,
+                TenantsSkeletonStorageCommand::class,
+            ]);
     }
 
-    public function boot(): void
+    public function packageBooted(): void
     {
         Event::listen(
             MadeTenantCurrentEvent::class,
@@ -41,34 +44,6 @@ class TenancyServiceProvider extends MultitenancyServiceProvider
         $router->aliasMiddleware('clean-cache', AppCleanCache::class);
         $this->app['router']->post('/clean-cache', ['uses' =>  TenantController::class.'@clean', 'as' => 'app.clean']);
 
-        if ($this->app->runningInConsole()) {
-            $this->registerPublishables();
-        }
-
-        $this->bootCommands();
-
         parent::packageBooted();
-    }
-
-    public function register(): void
-    {
-        $this->mergeConfigFrom(__DIR__.'/../config/multitenancy.php', 'multitenancy');
-    }
-
-    protected function registerPublishables(): self
-    {
-        $this->publishes([
-            __DIR__.'/../config/multitenancy.php' => config_path('multitenancy.php'),
-        ], 'config');
-
-        if (! class_exists('CreateLandlordTenantsTable')) {
-            $this->publishes([
-                __DIR__.'/../database/migrations/landlord/create_landlord_tenants_table.php.stub' => database_path('migrations/landlord/'.date('Y_m_d_His', time()).'_create_landlord_tenants_table.php'),
-                __DIR__.'/../database/migrations/landlord/create_landlord_jobs_table.php.stub' => database_path('migrations/landlord/'.date('Y_m_d_His', time()).'_create_landlord_jobs_table.php'),
-                __DIR__.'/../database/migrations/landlord/create_landlord_failed_jobs_table.php.stub' => database_path('migrations/landlord/'.date('Y_m_d_His', time()).'_create_landlord_failed_jobs_table.php'),
-            ], 'migrations');
-        }
-
-        return $this;
     }
 }
