ccc2ee13152f11b741d4f21a59fba4651afdb26e
Ivan Andres L <ivan.lopez@evertecinc.com>
2024-04-03 09:25:51 -0500

    upgrade friendsofphp/php-cs-fixer to version 3.0 and  adds eduarguz/shift-php-cs package

---
+++ .php-cs-fixer.php
@@ -0,0 +1,22 @@
+<?php
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->notPath('bootstrap')
+    ->notPath('storage')
+    ->notPath('packages')
+    ->notPath('vendor')
+    ->in(getcwd())
+    ->name('*.php')
+    ->notName('*.blade.php')
+    ->notName('index.php')
+    ->notName('_ide_helper.php')
+    ->notName('server.php')
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true);
+
+return \ShiftCS\styles($finder, [
+    'no_unused_imports' => true,
+]);
