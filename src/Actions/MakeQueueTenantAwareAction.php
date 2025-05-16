6b5b146830ad3efd059bafeb2c8c461ebd58a707
Iván Andrés López G. <andresgom11012@gmail.com>
2025-05-16 01:19:56 -0500

    wip

--- src/Actions/MakeQueueTenantAwareAction.php
+++ src/Actions/MakeQueueTenantAwareAction.php
@@ -11,6 +11,32 @@ use Spatie\Multitenancy\Exceptions\CurrentTenantCouldNotBeDeterminedInTenantAwar
 
 class MakeQueueTenantAwareAction extends \Spatie\Multitenancy\Actions\MakeQueueTenantAwareAction
 {
+    protected function listenForJobsBeingProcessed(): static
+    {
+        app('events')->listen(JobProcessing::class, function (JobProcessing $event) {
+            if (! array_key_exists('tenantDomain', $event->job->payload())) {
+                return;
+            }
+
+            $this->findTenant($event)->makeCurrent();
+        });
+
+        return $this;
+    }
+
+    protected function listenForJobsRetryRequested(): static
+    {
+        app('events')->listen(JobRetryRequested::class, function (JobRetryRequested $event) {
+            if (! array_key_exists('tenantDomain', $event->payload())) {
+                return;
+            }
+
+            $this->findTenant($event)->makeCurrent();
+        });
+
+        return $this;
+    }
+
     protected function findTenant(JobProcessing|JobRetryRequested $event): IsTenant
     {
         $tenantDomain = $this->getEventPayload($event)['tenantDomain'] ?? null;
