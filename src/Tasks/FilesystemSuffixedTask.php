1859ff2ab3ce64e4937e4899c7577b2a3d8ffb59
Alejandro Alvarez <alejandro.alvarez@evertecinc.com>
2026-08-21 15:53:58 -0500

    refactor: revert property promotion

--- src/Tasks/FilesystemSuffixedTask.php
+++ src/Tasks/FilesystemSuffixedTask.php
@@ -10,10 +10,13 @@ use Spatie\Multitenancy\Tasks\SwitchTenantTask;
 
 class FilesystemSuffixedTask implements SwitchTenantTask
 {
+    private Application $app;
+
     private array $originalPaths;
 
-    public function __construct(private Application $app)
+    public function __construct(Application $app)
     {
+        $this->app = $app;
         $this->originalPaths = [
             'disks' => [],
             'storage' => $this->app->storagePath(),
